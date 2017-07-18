65297d32d372367e49a2113a707a1e16a1cd6ca1
katiaas <katiaas@users.noreply.github.com>
2017-07-18 02:09:06 +0200

    Addon works for multiple realms

--- addon/GoldGoblin/GoldGoblin.lua
+++ addon/GoldGoblin/GoldGoblin.lua
@@ -1,15 +1,14 @@
 SLASH_GOLDGOBLIN1, SLASH_GOLDGOBLIN2 = '/gg', '/goldgoblin';
 
 function SlashCmdList.GOLDGOBLIN(msg, editbox)
-	if msg == "" then
-		for k, v in ipairs(GoldGoblinData) do
+	if GoldGoblinData[GetRealmName()] ~= nil then
+		DEFAULT_CHAT_FRAME:AddMessage("Prospecting data for |cFFFFFF00" .. GetRealmName());
+		for k, v in ipairs(GoldGoblinData[GetRealmName()]) do
 			for key, value in pairs(v) do
 				DEFAULT_CHAT_FRAME:AddMessage("|cFF00FF00" .. key .. ": |r" .. value);
 			end
 		end
-	elseif GoldGoblinData[msg] ~= nil then
-		DEFAULT_CHAT_FRAME:AddMessage(GoldGoblinData[msg]);
 	else
-		DEFAULT_CHAT_FRAME:AddMessage('GoldGoblin did not recognize command \'' .. msg .. '\'');
+		DEFAULT_CHAT_FRAME:AddMessage("There is no known data for this realm. Sorry :(");
 	end
 end
--- addon/GoldGoblin/GoldGoblinData.lua
+++ addon/GoldGoblin/GoldGoblinData.lua
@@ -1,9 +1,20 @@
 GoldGoblinData = {
-	[1] = {["Colors"] = "red,green"},
-	[2] = {["Total Cost"] = "12685g"},
-	[3] = {["Profit"] = "126g"},
-	[4] = {["Felslate Amount"] = "1000"},
-	[5] = {["Felslate Cost (ea.)"] = "28g"},
-	[6] = {["Leystone Amount"] = "1000"},
-	[7] = {["Leystone Cost (ea.)"] = "10g"},
+	["Ragnaros"] = {
+		[1] = {["Colors"] = "red,green"},
+		[2] = {["Total Cost"] = "12685g"},
+		[3] = {["Profit"] = "126g"},
+		[4] = {["Felslate Amount"] = "1000"},
+		[5] = {["Felslate Cost (ea.)"] = "28g"},
+		[6] = {["Leystone Amount"] = "1000"},
+		[7] = {["Leystone Cost (ea.)"] = "10g"},
+	},
+	["Outland"] = {
+		[1] = {["Colors"] = "red,green"},
+		[2] = {["Total Cost"] = "12685g"},
+		[3] = {["Profit"] = "126g"},
+		[4] = {["Felslate Amount"] = "1000"},
+		[5] = {["Felslate Cost (ea.)"] = "28g"},
+		[6] = {["Leystone Amount"] = "1000"},
+		[7] = {["Leystone Cost (ea.)"] = "10g"},
+	},
 };
